964dde937b94f30eec05853c6dd7bbb2893724f6
Anup Saud <anupsaud10@gmail.com>
2023-08-11 00:48:02 -0400

    Add register,login auth

---
+++ js/login.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+// Firebase configuration (replace with your actual Firebase configuration)
+const firebaseConfig = {
+    apiKey: "AIzaSyDkLIgwVKpqdOpuhF1-26RsKaRQmjMWtaw",
+    authDomain: "map-pwa-ca20f.firebaseapp.com",
+    databaseURL: "https://map-pwa-ca20f-default-rtdb.firebaseio.com",
+    projectId: "map-pwa-ca20f",
+    storageBucket: "map-pwa-ca20f.appspot.com",
+    messagingSenderId: "134536483378",
+    appId: "1:134536483378:web:1eb239d2da1ce7fb35222b",
+    measurementId: "G-PMZXDZ5N71"
+  };
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  
+  // Add a submit event listener to the login form
+  document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.querySelector('.login-form');
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault(); // Prevent form submission
+  
+      // Get form values
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      try {
+        // Sign in the user with the provided email and password
+        await signInWithEmailAndPassword(auth, email, password);
+        
+        // User successfully logged in
+        alert('Login successful!');
+  
+        // Redirect to recipe.html
+        window.location.href = 'recipe.html';
+      } catch (error) {
+        // Handle error
+        alert('Invalid email or password');
+        console.error('Error logging in:', error);
+      }
+    });
+  });
---
+++ js/recipe.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore, collection, getDocs, query } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkLIgwVKpqdOpuhF1-26RsKaRQmjMWtaw",
+    authDomain: "map-pwa-ca20f.firebaseapp.com",
+    databaseURL: "https://map-pwa-ca20f-default-rtdb.firebaseio.com",
+    projectId: "map-pwa-ca20f",
+    storageBucket: "map-pwa-ca20f.appspot.com",
+    messagingSenderId: "134536483378",
+    appId: "1:134536483378:web:1eb239d2da1ce7fb35222b",
+    measurementId: "G-PMZXDZ5N71"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const userContainer = document.getElementById('user-container');
+    const userIcon = document.getElementById('user-icon');
+    const logoutButton = document.getElementById('logout-button');
+    const userName = document.getElementById('user-name');
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is logged in, show user info and logout option
+            userContainer.style.display = 'flex';
+            userName.textContent = user.displayName;
+
+            // Handle logout
+            logoutButton.addEventListener('click', () => {
+                signOut(auth)
+                    .then(() => {
+                        // Redirect to home page
+                        window.location.href = '/index.html'; 
+                    })
+                    .catch((error) => {
+                        console.error('Error logging out:', error);
+                    });
+            });
+        } else {
+            // User is not logged in, hide user info and logout option
+            userContainer.style.display = 'none';
+            userName.textContent = '';
+        }
+    });
+
+  
+    try {
+        const recipesQuery = query(collection(db, 'recipes'));
+        const recipesSnapshot = await getDocs(recipesQuery);
+        recipesSnapshot.forEach((doc) => {
+            // Display each recipe on the page
+            const recipeData = doc.data();
+            console.log('Recipe:', recipeData);
+        });
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+    }
+});
